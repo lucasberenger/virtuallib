d398641b82f80b2cb821ac957b9a0dcd3cbe44a7
Lucas <thegreatberenger@gmail.com>
2024-03-11 10:41:27 -0300

    add admin and first database content

--- library/admin.py
+++ library/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Books
 
-# Register your models here.
+class BooksAdmin(admin.ModelAdmin):
+    pass  
+
+admin.site.register(Books, BooksAdmin)
+
--- library/models.py
+++ library/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+class Books(models.Model):
+    title = models.CharField(max_length=30, blank=False, null=False)
+    author = models.CharField(max_length=30, blank=False, null=False)
+    year = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+     
+
+
 
